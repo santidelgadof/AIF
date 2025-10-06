ad4ffdbe504c2266bbf837fecd656b832530ca57
Santiago Delgado Ferreiro <santiago.delgadof@udc.es>
2025-10-06 19:01:23 +0200

    Main actualizado

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 from problem import load_map
-from search import bfs, dfs, astar
+from search import bfs, dfs, astar, heuristic
 
 if __name__ == "__main__":
     if len(sys.argv) < 3:
@@ -24,21 +24,27 @@ if __name__ == "__main__":
         print("Algoritmo no reconocido (usa bfs, dfs o astar)")
         sys.exit(1)
 
-    if path:        
-        # Primer nodo (inicial)
+    if path:
+        # Nodo 0
         state0, g0, d0, op0 = path[0]
-        if verbose:
-            print("\n\n-------------------------------------------------\n\n")
         print(f"\nNode 0 (starting node)")
-        print(f"  ({d0}, {g0}, {op0}, {state0})")
-        
-        # Resto de nodos con sus operadores
+        if algo == "astar":
+            h0 = heuristic(state0, goal)
+            print(f"  ({d0}, {g0}, {op0}, {h0:.2f}, {state0})")
+        else:
+            print(f"  ({d0}, {g0}, {op0}, {state0})")
+
+        # Resto de nodos
         for i in range(1, len(path)):
             state, g, d, op = path[i]
             print(f"\nOperator {i}")
             print(f"  {op}")
             print(f"Node {i}")
-            print(f"  ({d}, {g}, {op}, {state})")
+            if algo == "astar":
+                hi = heuristic(state, goal)
+                print(f"  ({d}, {g}, {op}, {hi:.2f}, {state})")
+            else:
+                print(f"  ({d}, {g}, {op}, {state})")
         
         # Estadísticas finales
         print(f"\nTotal number of items in explored list: {len(explored)}")
